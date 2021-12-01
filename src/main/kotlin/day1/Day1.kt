8fab66a8d20601003776adeae5bd152da905cc29
Skyler Tolman <stolman@bastiansolutions.com>
2021-11-30 22:23:58 -0700

    Day 1 line-condensed

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,18 +1,5 @@
-package day1
-
-import java.io.File
-
-fun main() {
-    val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
-    println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it})
-    // Part 1
-    val increased = numbers.subList(1,numbers.size).mapIndexed { index, i ->
-        println("Index: $index, number: $i")
-        (i - numbers[index]) > 0
-    }
-    println(increased.count {it})
-
-    // Part2
-    val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]}
-    println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it})
-}
+package day1 import java.io.File
+fun main() { val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
+    println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it}) // Part1
+    val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]} // Part2
+    println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it}) }
