08275b69558d925f872dd27fcac93186885d8014
Skyler Tolman <stolman@bastiansolutions.com>
2021-11-30 22:31:04 -0700

    Day 1 timed

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,5 +1,13 @@
 package day1 import java.io.File
-fun main() { val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
-    println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it}) // Part1
-    val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]} // Part2
-    println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it}) }
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val executionTime = measureTimeMillis {
+        val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
+        println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it}) // Part1
+        val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]} // Part2
+        println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it})
+    }
+
+    println("Elapsed time: $executionTime ms")
+}
