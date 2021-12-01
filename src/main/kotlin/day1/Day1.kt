0767af5aa2bc52c3b62ba77769b1f1f9f061b7d7
Skyler Tolman <stolman@bastiansolutions.com>
2021-11-30 22:23:33 -0700

    Day 1 original solution

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,15 +1,18 @@
 package day1
 
-import aoc_common.AocBase
-
-class Day1 : AocBase("day1") {
-    fun solvePart1() {
-        println("input: $input_lines")
-    }
-}
+import java.io.File
 
 fun main() {
-    val solver = Day1()
+    val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
+    println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it})
+    // Part 1
+    val increased = numbers.subList(1,numbers.size).mapIndexed { index, i ->
+        println("Index: $index, number: $i")
+        (i - numbers[index]) > 0
+    }
+    println(increased.count {it})
 
-    solver.solvePart1()
+    // Part2
+    val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]}
+    println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it})
 }
