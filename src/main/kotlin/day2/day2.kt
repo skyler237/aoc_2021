1ab500f7d8e36731a7de4fab91b058e3e98f1fb3
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-01 22:36:47 -0700

    Back to proper formatting

--- src/main/kotlin/day2/day2.kt
+++ src/main/kotlin/day2/day2.kt
@@ -1,10 +1,27 @@
-package day2 import java.io.File
-fun main(){var depth=0
-var aim=0
-var position=0
-File("src/main/kotlin/day2/input.txt").readLines().forEach{val(command,value)=it.split(" ")
-with(value.toInt()){when(command){"forward"->{position+=this
-depth+=aim * this}
-"up"->{aim-=this}
-"down"->{aim+=this}}}}
-println(depth*position)}
+package day2
+
+import java.io.File
+
+fun main() {
+    var depth = 0
+    var aim = 0
+    var position = 0
+    File("src/main/kotlin/day2/input.txt").readLines().forEach {
+        val (command, value) = it.split(" ")
+        with(value.toInt()) {
+            when (command) {
+                "forward" -> {
+                    position += this
+                    depth += aim * this
+                }
+                "up" -> {
+                    aim -= this
+                }
+                "down" -> {
+                    aim += this
+                }
+            }
+        }
+    }
+    println(depth * position)
+}
