3f089c668f19083798434a9417624b239abe7b31
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-02 09:58:03 -0700

    Day 1 back to decent formatting

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,5 +1,11 @@
-package day1 import java.io.File
-fun main(){with(File("src/main/kotlin/day1/input.txt").readLines().map{it.toInt()}){
-println(this.subList(1,this.size).mapIndexed{i,x->(x-this[i])>0}.count{it})
-val sums=this.subList(0,this.size-2).mapIndexed{i,x->x+this[i+1]+this[i+2]}
-println(sums.subList(0,sums.size-1).mapIndexed{i,x->sums[i+1]-x>0}.count{it})}}
+package day1
+
+import java.io.File
+
+fun main() {
+    with(File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }) {
+        println(this.subList(1, this.size).mapIndexed { i, x -> (x - this[i]) > 0 }.count { it })
+        val sums = this.subList(0, this.size - 2).mapIndexed { i, x -> x + this[i + 1] + this[i + 2] }
+        println(sums.subList(0, sums.size - 1).mapIndexed { i, x -> sums[i + 1] - x > 0 }.count { it })
+    }
+}
