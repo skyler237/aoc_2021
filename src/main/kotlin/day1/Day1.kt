fb77d4fed64ad25b636242c5951b4eae3dbb862e
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-02 09:56:59 -0700

    Day 1 code golf

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,13 +1,5 @@
 package day1 import java.io.File
-import kotlin.system.measureTimeMillis
-
-fun main() {
-    val executionTime = measureTimeMillis {
-        val numbers = File("src/main/kotlin/day1/input.txt").readLines().map { it.toInt() }
-        println(numbers.subList(1,numbers.size).mapIndexed { index, i -> (i - numbers[index]) > 0 }.count{it}) // Part1
-        val sums = numbers.subList(0,numbers.size-2).mapIndexed {index, i ->  i + numbers[index+1] + numbers[index+2]} // Part2
-        println(sums.subList(0,sums.size-1).mapIndexed { index, i -> sums[index + 1] - i > 0}.count{it})
-    }
-
-    println("Elapsed time: $executionTime ms")
-}
+fun main(){with(File("src/main/kotlin/day1/input.txt").readLines().map{it.toInt()}){
+println(this.subList(1,this.size).mapIndexed{i,x->(x-this[i])>0}.count{it})
+val sums=this.subList(0,this.size-2).mapIndexed{i,x->x+this[i+1]+this[i+2]}
+println(sums.subList(0,sums.size-1).mapIndexed{i,x->sums[i+1]-x>0}.count{it})}}
