52d3bc25683c2ff66dc1988d3d9e5b849255de7f
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-01 22:34:44 -0700

    Code golf, part2 only

--- src/main/kotlin/day2/day2.kt
+++ src/main/kotlin/day2/day2.kt
@@ -1,46 +1,10 @@
-package day2
-import java.io.File
-import kotlin.system.measureTimeMillis
-
-fun main() {
-    val executionTime = measureTimeMillis {
-        var depth = 0
-        var position = 0
-        var aim = 0
-        File("src/main/kotlin/day2/input.txt").readLines().forEach{
-            val (command, value) = it.split(" ")
-            with(value.toInt()) {
-                // Part 1
-//                when (command) {
-//                    "forward" -> {
-//                        position += this
-//                    }
-//                    "up" -> {
-//                        depth -= this
-//                    }
-//                    "down" -> {
-//                        depth += this
-//                    }
-//                }
-//                println(depth * position)
-                // Part 2
-                when (command) {
-                    "forward" -> {
-                        position += this
-                        depth += aim * this
-                    }
-                    "up" -> {
-                        aim -= this
-                    }
-                    "down" -> {
-                        aim += this
-                    }
-                }
-            }
-        }
-        println(depth * position)
-
-    }
-
-    println("Elapsed time: $executionTime ms")
-}
+package day2 import java.io.File
+fun main(){var depth=0
+var aim=0
+var position=0
+File("src/main/kotlin/day2/input.txt").readLines().forEach{val(command,value)=it.split(" ")
+with(value.toInt()){when(command){"forward"->{position+=this
+depth+=aim * this}
+"up"->{aim-=this}
+"down"->{aim+=this}}}}
+println(depth*position)}
