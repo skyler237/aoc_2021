1c09f024fc25377425545087986fb737a5190805
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-07 07:26:40 -0700

    Day 6 code golf - 271 chars

--- src/main/kotlin/day6/day6.kt
+++ src/main/kotlin/day6/day6.kt
@@ -1,21 +1,8 @@
 package day6 import java.io.File
-import kotlin.system.measureTimeMillis
-
-const val fishStr = "3,4,1,2,1,2,5,1,2,1,5,4,3,2,5,1,5,1,2,2,2,3,4,5,2,5,1,3,3,1,3,4,1,5,3,2,2,1,3,2,5,1,1,4,1,4,5,1,3,1,1,5,3,1,1,4,2,2,5,1,5,5,1,5,4,1,5,3,5,1,1,4,1,2,2,1,1,1,4,2,1,3,1,1,4,5,1,1,1,1,1,5,1,1,4,1,1,1,1,2,1,4,2,1,2,4,1,3,1,2,3,2,4,1,1,5,1,1,1,2,5,5,1,1,4,1,2,2,3,5,1,4,5,4,1,3,1,4,1,4,3,2,4,3,2,4,5,1,4,5,2,1,1,1,1,1,3,1,5,1,3,1,1,2,1,4,1,3,1,5,2,4,2,1,1,1,2,1,1,4,1,1,1,1,1,5,4,1,3,3,5,3,2,5,5,2,1,5,2,4,4,1,5,2,3,1,5,3,4,1,5,1,5,3,1,1,1,4,4,5,1,1,1,3,1,4,5,1,2,3,1,3,2,3,1,3,5,4,3,1,3,4,3,1,2,1,1,3,1,1,3,1,1,4,1,2,1,2,5,1,1,3,5,3,3,3,1,1,1,1,1,5,3,3,1,1,3,4,1,1,4,1,1,2,4,4,1,1,3,1,3,2,2,1,2,5,3,3,1,1"
-
-fun main() {
-    val time = measureTimeMillis {
-        val fish = fishStr.split(',').map { it.toInt() }
-        val fishCounts = LongArray(9)
-        fish.forEach { fishCounts[it] = fishCounts[it] + 1 }
-        for (i in 0 until 256) {
-            val zeroIndex = i % 9
-            val sevenIndex = (zeroIndex + 7) % 9
-            fishCounts[sevenIndex] += fishCounts[zeroIndex]
-            if (i == 79 || i == 255) {
-                println(fishCounts.sum())
-            }
-        }
-    }
-    println(time)
-}
+fun main(){val x=File("src/main/kotlin/day6/input.txt").readText().trim().split(',').map{it.toInt()}
+val y=LongArray(9)
+x.forEach{y[it]+=y[it]+1}
+for(i in 0 until 256){val a=i%9
+val b=(a+7)%9
+y[b]+=y[a]
+if(i==79||i==255)println(y.sum())}}
