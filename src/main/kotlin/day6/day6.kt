02904009115efd63c69f9495e1ff3d80ec86c665
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-07 07:27:29 -0700

    Day 6 back to readable

--- src/main/kotlin/day6/day6.kt
+++ src/main/kotlin/day6/day6.kt
@@ -1,8 +1,14 @@
 package day6 import java.io.File
-fun main(){val x=File("src/main/kotlin/day6/input.txt").readText().trim().split(',').map{it.toInt()}
-val y=LongArray(9)
-x.forEach{y[it]+=y[it]+1}
-for(i in 0 until 256){val a=i%9
-val b=(a+7)%9
-y[b]+=y[a]
-if(i==79||i==255)println(y.sum())}}
+fun main() {
+    val fish=File("src/main/kotlin/day6/input.txt").readText().trim().split(',').map { it.toInt() }
+    val fishCounts = LongArray(9)
+    fish.forEach { fishCounts[it] = fishCounts[it] + 1 }
+    for( i in 0 until 256) {
+        val zeroIndex = i % 9
+        val sevenIndex = (zeroIndex + 7) % 9
+        fishCounts[sevenIndex] += fishCounts[zeroIndex]
+        if (i == 79 || i == 255) {
+            println(fishCounts.sum())
+        }
+    }
+}
