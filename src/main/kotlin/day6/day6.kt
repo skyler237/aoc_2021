45a001c11f2c8487982a6f3bde4453bb048bfc69
Skyler Tolman <stolman@bastiansolutions.com>
2021-12-07 07:18:54 -0700

    Day 6 execution time - 29ms

--- src/main/kotlin/day6/day6.kt
+++ src/main/kotlin/day6/day6.kt
@@ -1,27 +1,21 @@
-package day6
+package day6 import java.io.File
+import kotlin.system.measureTimeMillis
 
-import java.io.File
+const val fishStr = "3,4,1,2,1,2,5,1,2,1,5,4,3,2,5,1,5,1,2,2,2,3,4,5,2,5,1,3,3,1,3,4,1,5,3,2,2,1,3,2,5,1,1,4,1,4,5,1,3,1,1,5,3,1,1,4,2,2,5,1,5,5,1,5,4,1,5,3,5,1,1,4,1,2,2,1,1,1,4,2,1,3,1,1,4,5,1,1,1,1,1,5,1,1,4,1,1,1,1,2,1,4,2,1,2,4,1,3,1,2,3,2,4,1,1,5,1,1,1,2,5,5,1,1,4,1,2,2,3,5,1,4,5,4,1,3,1,4,1,4,3,2,4,3,2,4,5,1,4,5,2,1,1,1,1,1,3,1,5,1,3,1,1,2,1,4,1,3,1,5,2,4,2,1,1,1,2,1,1,4,1,1,1,1,1,5,4,1,3,3,5,3,2,5,5,2,1,5,2,4,4,1,5,2,3,1,5,3,4,1,5,1,5,3,1,1,1,4,4,5,1,1,1,3,1,4,5,1,2,3,1,3,2,3,1,3,5,4,3,1,3,4,3,1,2,1,1,3,1,1,3,1,1,4,1,2,1,2,5,1,1,3,5,3,3,3,1,1,1,1,1,5,3,3,1,1,3,4,1,1,4,1,1,2,4,4,1,1,3,1,3,2,2,1,2,5,3,3,1,1"
 
 fun main() {
-    val fish = File("src/main/kotlin/day6/input.txt").readText().trim().split(',').map { it.toInt() }
-    val fishCounts = LongArray(9)
-    fish.forEach { fishCounts[it] = fishCounts[it] + 1 }
-    for( i in 0 until 256) {
-        val zeroIndex = i % 9
-        val sevenIndex = (zeroIndex + 7) % 9
-        fishCounts[sevenIndex] += fishCounts[zeroIndex]
-        if (i == 79 || i == 255) {
-            println(fishCounts.sum())
+    val time = measureTimeMillis {
+        val fish = fishStr.split(',').map { it.toInt() }
+        val fishCounts = LongArray(9)
+        fish.forEach { fishCounts[it] = fishCounts[it] + 1 }
+        for (i in 0 until 256) {
+            val zeroIndex = i % 9
+            val sevenIndex = (zeroIndex + 7) % 9
+            fishCounts[sevenIndex] += fishCounts[zeroIndex]
+            if (i == 79 || i == 255) {
+                println(fishCounts.sum())
+            }
         }
     }
+    println(time)
 }
-
-/*
-0 -> 80 =
-6,79 + 8,79 =
-5,78 + 7,78 =
-0,73 + 2,73 =
-6,72 + 1,72 + 8,72 =
-5,71 + 0,71 + 7,71 =
-4,70 + 6,70 + 6,70 + 8,70
- */
